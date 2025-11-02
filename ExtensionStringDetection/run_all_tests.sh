c4579ff665824e15387e70ceb9c583b3236528b9
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-11-02 00:25:48 -0400

    Update run_all_tests.sh

--- ExtensionStringDetection/run_all_tests.sh
+++ ExtensionStringDetection/run_all_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for ext in pif lnk exe docx xlsx pptx pdf rar 7z zip dll rtf html htm; do
-    yara detect_text_for_${ext}_string.yar TestTextFile.txt
+    yara64 detect_text_for_${ext}_string.yar TestTextFile.txt
 done
 
 
