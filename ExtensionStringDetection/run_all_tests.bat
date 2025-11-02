9197b6e6be1b1050ce1d27c26ee736110703d366
Savannah C <113316987+savannahc502@users.noreply.github.com>
2025-11-02 00:21:46 -0400

    Update run_all_tests.bat

--- ExtensionStringDetection/run_all_tests.bat
+++ ExtensionStringDetection/run_all_tests.bat
@@ -1,4 +1,4 @@
 @echo off
 for %%E in (pif lnk exe docx xlsx pptx pdf rar 7z zip dll rtf html htm) do (
-    yara detect_text_for_%%E_string.yar TestTextFile.txt
+    "C:\Users\champuser\Downloads\ExtensionStringDetection\yara64.exe" detect_text_for_%%E_string.yar TestTextFile.txt
 )
